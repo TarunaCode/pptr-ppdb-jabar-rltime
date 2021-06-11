418b2aa81ec0f9d1c38b81033312cde512be24a0
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-11 11:04:47 +0700

    Menambahkan getSchools

---
+++ tasks/getSchools.js
@@ -0,0 +1,57 @@
+const pptr = require("puppeteer-core");
+const path = require("path");
+const fs = require("fs");
+const {
+  CITY,
+  TYPE,
+  LEVEL,
+  EXPATH,
+  UDDIR,
+  INFO_DIR,
+} = require("../lib/constant");
+const { requestWrapper } = require("../lib");
+
+const schoolLists = (city, type, level) =>
+  `https://api.ppdb.disdik.jabarprov.go.id/portal/school?pagination=false&filters[0][key]=address_city&filters[0][value]=${encodeURI(
+    city.toUpperCase().trim()
+  )}&filters[1][key]=type&filters[1][value]=${type
+    .toLowerCase()
+    .trim()}&filters[2][key]=level&filters[2][value]=${level
+    .toLowerCase()
+    .trim()}`;
+
+if (!fs.existsSync(INFO_DIR)) fs.mkdirSync(INFO_DIR);
+
+(async () => {
+  try {
+    const mainURL = schoolLists(CITY, TYPE, LEVEL);
+
+    const browser = await pptr.launch({
+      executablePath: EXPATH,
+      userDataDir: UDDIR,
+      // headless: false,
+    });
+
+    const page = await browser.newPage();
+    const req = requestWrapper(page);
+
+    const schools = await req(mainURL);
+
+    const data = schools.result.itemsList.map((school) => ({
+      id: school.id,
+      name: school.name,
+    }));
+
+    fs.writeFileSync(
+      path.join(
+        INFO_DIR,
+        `schools-${CITY.replace(/\s/g, "_")}-${TYPE}-${LEVEL}.json`
+      ),
+      JSON.stringify(data, null, 2)
+    );
+
+    await browser.close();
+  } catch (e) {
+    console.error(e);
+  }
+})();
