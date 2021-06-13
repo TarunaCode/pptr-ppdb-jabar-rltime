a84beb1a1c9c0edfb3583ccf05bac44ac1d3d6f1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-13 15:41:09 +0700

    Memperbaiki pengambilan data info

--- tasks/pdfReportRegistrant.js
+++ tasks/pdfReportRegistrant.js
@@ -12,13 +12,23 @@ const {
 } = require("../lib/constant");
 const { requestWrapper } = require("../lib");
 
+let data = [];
+
 const html = fs.readFileSync(path.join(MAINDIR, "views", "rgstn.html"), "utf8");
 const schools = fs
   .readdirSync(INFO_DIR)
   .filter((sch) => sch.includes("schools"));
 
-const schoolsData = require(path.join(INFO_DIR, schools[0]));
-const school = schoolsData.find((data) => data.id === SCH_ID);
+schools.forEach((filename) => {
+  const content = fs.readFileSync(path.join(INFO_DIR, filename), "utf8");
+  const parsed = JSON.parse(content);
+
+  parsed.forEach((info) => {
+    data.push(info);
+  });
+});
+
+const school = data.find((data) => data.id === SCH_ID);
 
 const registrantUrl = (schoolID, option_type) =>
   `https://api.ppdb.disdik.jabarprov.go.id/portal/registrant?pagination=false&orderby=created_at&order=asc&search=&columns[0][key]=name&columns[0][searchable]=true&columns[1][key]=registration_number&columns[1][searchable]=true&filters[0][key]=first_choice_school&filters[0][value]=${schoolID}&filters[1][key]=option_type&filters[1][value]=${option_type}`;
